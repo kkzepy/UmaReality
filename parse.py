16950404f70fbfc7663595f63d491392e7bd5fbc
kkzepy <keinontokaze@gmail.com>
2026-04-30 13:12:00 +0700

    motset

--- parse.py
+++ parse.py
@@ -1,4 +1,4 @@
-import re
+import re, json
 
 motion_set_data = {
     "motion_set":{
@@ -6,79 +6,79 @@ motion_set_data = {
     }
 }
 
-"""
- idea: idea
- hope: hope
- fight: excited
- soppo: facing-away
- sad: sad
- muneate: hand-on-chest
- think: thinking
- shy: shy
- tekumi: hands-intertwined
- surprise: surprised
- kuchiate: covering-mouth
- hello: greeting
- handsup: hands-up
- bow: bow
- sneeze: sneeze
- idle: idle
- sleep: sleep
- guard: guard-self
- sudachi: stand-straight
- joy: happy
- koshiate: hands-on-hip
- angry: angry
- shock: shock
- cry: cry
- gatts: confident
- pose: pose
- akire: idk
- near: leaning-in
- udekumi: folded-arms
- byebye: farewell
- tired: tired
- laugh: laughing
- good: thumbs-up
- peace: peace
- happy: happy
- advice: advice
- clap: hands-clap
- mot: NONE
- pain: pain
- send: lending-hands
- quiet: be-quiet
- homestand: NONE
- hungry: hungry
- see: seeing-away
- ikaku: ready
- come: come-closer
- point: point
- stretch: stretch
- hot: hot
- neckup: neckup-nod
- oh: excited
- yell: yell
- joke: joking
- swing: swinging-hands
- shake: shake
- sorry: sorry
- ng: no-gesture
- muscle: muscle
- salute: salute
- pdk: NONE
- stop: stop
- request: please
- set: NONE
- kiss: NONE
- stomp: stomp
- hurry: hurry
- act: NONE
- diarywrite: write_diary
- check: NONE
- watchbase: NONE
- watchlook: NONE
-"""
+keys = {#{"fight":"excited","kuchiate":"covering_mouth","muneate":"hand_on_chest","sad":"sad","tekumi":"hands_intertwined","tired":"tired","think":"thinking","soppo":"facing_away","guard":"guard_self","see":"seeing_away","bow":"bow","come":"come_closer","hope":"hope",
+ " idea": "idea",
+ " hope": "hope",
+ " fight": "excited",
+ " soppo": "facing-away",
+ " sad": "sad",
+ " muneate": "hand-on-chest",
+ " think": "thinking",
+ " shy": "shy",
+ " tekumi": "hands-intertwined",
+ " surprise": "surprised",
+ " kuchiate": "covering-mouth",
+ " hello": "greeting",
+ " handsup": "hands-up",
+ " bow": "bow",
+ " sneeze": "sneeze",
+ " idle": "idle",
+ " sleep": "sleep",
+ " guard": "guard-self",
+ " sudachi": "stand-straight",
+ " joy": "happy",
+ " koshiate": "hands-on-hip",
+ " angry": "angry",
+ " shock": "shock",
+ " cry": "cry",
+ " gatts": "confident",
+ " pose": "pose",
+ " akire": "idk",
+ " near": "leaning-in",
+ " udekumi": "folded-arms",
+ " byebye": "farewell",
+ " tired": "tired",
+ " laugh": "laughing",
+ " good": "thumbs-up",
+ " peace": "peace",
+ " happy": "happy",
+ " advice": "advice",
+ " clap": "hands-clap",
+ " mot": None,
+ " pain": "pain",
+ " send": "lending-hands",
+ " quiet": "be-quiet",
+ " homestand": None,
+ " hungry": "hungry",
+ " see": "seeing-away",
+ " ikaku": "ready",
+ " come": "come-closer",
+ " point": "point",
+ " stretch": "stretch",
+ " hot": "hot",
+ " neckup": "neckup-nod",
+ " oh": "excited",
+ " yell": "yell",
+ " joke": "joking",
+ " swing": "swinging-hands",
+ " shake": "shake",
+ " sorry": "sorry",
+ " ng": "no-gesture",
+ " muscle": "muscle",
+ " salute": "salute",
+ " pdk": None,
+ " stop": "stop",
+ " request": "please",
+ " set": None,
+ " kiss": None,
+ " stomp": "stomp",
+ " hurry": "hurry",
+ " act": None,
+ " diarywrite": "write_diary",
+ " check": None,
+ " watchbase": None,
+ " watchlook": None
+}
 
 def CollectAllMotset():
     with open("motset.txt","r") as motset_file:
@@ -89,6 +89,7 @@ def CollectAllMotset():
             char_id = int(motset[1][:4])
             #if char_id != 1025: continue
             
+            """
             data = {
                     "excited":[], #fight
                     "covering_mouth":[], #kuchiate
@@ -104,16 +105,24 @@ def CollectAllMotset():
                     "come_closer":[], #come
                     "hope":[], #hope
             }
+            """
+            data = {}
             
             for line in motset:
                 line = line.split(",")
                 if len(line) == 1: continue
 
-                keys = {"fight":"excited","kuchiate":"covering_mouth","muneate":"hand_on_chest","sad":"sad","tekumi":"hands_intertwined","tired":"tired","think":"thinking","soppo":"facing_away","guard":"guard_self","see":"seeing_away","bow":"bow","come":"come_closer","hope":"hope"}
-
                 for k, v in keys.items():
                     if k in line[1]:
-                        data[v].append(f"3d/motion/event/body/type00/anm_eve_type00_{line[1][1:]}_loop")
+                        if not v in data:
+                            data[v]=[]
+
+                        path = f"3d/motion/event/body/type00/anm_eve_type00_{line[1][1:].replace('_mirror','')}_loop"
+                        if "_mirror" in line[1]:
+                            path+="_mirror"
+
+                        if not path in data[v]:
+                            data[v].append(path)
                         break
 
             motion_set_data[str(char_id)] = data
@@ -140,4 +149,6 @@ def AnimsOverrideKeys():
 
         print(animov_names)
 
-AnimsOverrideKeys()
+#AnimsOverrideKeys()
+CollectAllMotset()
+json.dump(motion_set_data, open("motset_final.json","w"), indent=4)
